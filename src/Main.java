7faadc425639b8486041ad1edec3102a7af752b6
arclyte1 <arclyte18@gmail.com>
2020-10-24 14:41:40 +0300

    Complited 1.1 task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Yarok");
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt(), b = in.nextInt(), c = a + b;
+        System.out.println("Сумма a + b = " + c);
     }
 }
